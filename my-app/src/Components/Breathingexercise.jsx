5e507cc6ab9a635577a73efc6202636498d747f9
Chikwa235 <chisapachikwa25@gmail.com>
2025-08-21 15:46:42 +0200

    feat: create BreathingExercise component with initial state

---
+++ my-app/src/Components/Breathingexercise.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+
+function BreathingExercise() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [seconds, setSeconds] = useState(0);
+  const [text, setText] = useState("Breathe in...");
+  const [scale, setScale] = useState(1);
+}
